ceecb1141168e8d0e52561d427fde09ecb0e6a3f
Mike Bowman <mbowman2@gmail.com>
2022-04-10 14:30:28 -0400

    Add dashboard in prod

--- lib/band_of_the_week_web/router.ex
+++ lib/band_of_the_week_web/router.ex
@@ -14,6 +14,10 @@ defmodule BandOfTheWeekWeb.Router do
     plug :accepts, ["json"]
   end
 
+  pipeline :admins_only do
+    plug :admin_basic_auth
+  end
+
   scope "/", BandOfTheWeekWeb do
     pipe_through :browser
 
@@ -22,6 +26,8 @@ defmodule BandOfTheWeekWeb.Router do
     scope "/spotify", Spotify do
       get "/authorize", AuthorizationController, :authorize
       get "/authenticate", AuthenticationController, :authenticate
+
+      live "/", BandOfTheWeekWeb.SpotifyLive
     end
   end
 
@@ -37,16 +43,20 @@ defmodule BandOfTheWeekWeb.Router do
   # If your application does not have an admins-only section yet,
   # you can use Plug.BasicAuth to set up some basic authentication
   # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
+  # if Mix.env() in [:dev, :test] do
+  import Phoenix.LiveDashboard.Router
 
-    scope "/" do
+  scope "/" do
+    if Mix.env() in [:dev, :test] do
       pipe_through :browser
-
-      live_dashboard "/dashboard", metrics: BandOfTheWeekWeb.Telemetry
-      live "/spotify", BandOfTheWeekWeb.SpotifyLive
+    else
+      pipe_through [:browser, :admins_only]
     end
+
+    live_dashboard "/dashboard", metrics: BandOfTheWeekWeb.Telemetry
   end
+
+  # end
 
   # Enables the Swoosh mailbox preview in development.
   #
@@ -58,5 +68,11 @@ defmodule BandOfTheWeekWeb.Router do
 
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  defp admin_basic_auth(conn, _opts) do
+    username = System.fetch_env!("AUTH_USERNAME")
+    password = System.fetch_env!("AUTH_PASSWORD")
+    Plug.BasicAuth.basic_auth(conn, username: username, password: password)
   end
 end
