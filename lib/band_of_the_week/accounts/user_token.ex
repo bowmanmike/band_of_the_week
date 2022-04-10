89e284d2d92bc9c4abfec07fc8b17d04fb1173c0
Mike Bowman <mbowman2@gmail.com>
2022-04-10 15:06:57 -0400

    Basic nav

--- lib/band_of_the_week/accounts/user_token.ex
+++ lib/band_of_the_week/accounts/user_token.ex
@@ -173,6 +173,7 @@ defmodule BandOfTheWeek.Accounts.UserToken do
   end
 
   def user_and_contexts_query(user, [_ | _] = contexts) do
-    from t in BandOfTheWeek.Accounts.UserToken, where: t.user_id == ^user.id and t.context in ^contexts
+    from t in BandOfTheWeek.Accounts.UserToken,
+      where: t.user_id == ^user.id and t.context in ^contexts
   end
 end
