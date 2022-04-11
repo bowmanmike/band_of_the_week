13f2983cbdb776bbadc8f76b5086016a80083401
Mike Bowman <mbowman2@gmail.com>
2022-04-11 16:37:47 -0400

    Progress on testing

--- test/support/factories/user_factory.ex
+++ test/support/factories/user_factory.ex
@@ -4,7 +4,8 @@ defmodule BandOfTheWeek.UserFactory do
       def user_factory do
         %BandOfTheWeek.Accounts.User{
           email: Faker.Internet.email(),
-          hashed_password: ""
+          password: "test_password_1",
+          hashed_password: "$2b$12$tx.U0eAdCXl0P.48qZyvqehXybAfSMO8ULnbhmzbwmJoI58LuIx9G"
         }
       end
     end
