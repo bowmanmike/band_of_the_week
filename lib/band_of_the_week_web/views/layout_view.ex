7c6961bad686459f73dd15b3c2e60223123b9d9c
Mike Bowman <mbowman2@gmail.com>
2022-04-12 14:15:55 -0400

    Refactor flashes to use loop

--- lib/band_of_the_week_web/views/layout_view.ex
+++ lib/band_of_the_week_web/views/layout_view.ex
@@ -4,4 +4,8 @@ defmodule BandOfTheWeekWeb.LayoutView do
   # Phoenix LiveDashboard is available only in development by default,
   # so we instruct Elixir to not warn if the dashboard route is missing.
   @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
+
+  def flash_color_for_level("info"), do: "green"
+  def flash_color_for_level("warning"), do: "yellow"
+  def flash_color_for_level("error"), do: "red"
 end
